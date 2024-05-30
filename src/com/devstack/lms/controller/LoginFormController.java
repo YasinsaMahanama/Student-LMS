3c2e5a54e257c9810fd870100576c0a09524b0e9
Yasinsa Mahanama <yasinsamahanama@gmail.com>
2024-05-30 18:46:38 +0530

    create Student LMS

---
+++ src/com/devstack/lms/controller/LoginFormController.java
@@ -0,0 +1,23 @@
+package com.devstack.lms.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class LoginFormController {
+
+    public AnchorPane context;
+
+    public void OpenDashboardOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/DashboardForm.fxml");
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene((new Scene(FXMLLoader.load(resource))));
+        stage.setTitle("Dashboard");
+        stage.centerOnScreen();
+    }
+}
---
+++ src/com/devstack/lms/db/DatabaseAccessCode.java
@@ -0,0 +1,9 @@
+package com.devstack.lms.db;
+
+import com.devstack.lms.model.Student;
+
+public class DatabaseAccessCode {
+    public boolean saveStudent(Student student) {
+        return false;
+    }
+}
