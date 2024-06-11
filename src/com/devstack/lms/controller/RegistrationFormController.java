bc4c36a834e6f6597c0cf7d3a3740adcab71a79c
Yasinsa Mahanama <yasinsamahanama@gmail.com>
2024-06-12 00:05:07 +0530

    updated

--- src/com/devstack/lms/controller/RegistrationFormController.java
+++ src/com/devstack/lms/controller/RegistrationFormController.java
@@ -3,17 +3,30 @@ package com.devstack.lms.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
 
-public class DashboardFormController {
+public class RegistrationFormController {
     public AnchorPane context;
+    public TextField txtStudentName;
+    public TextField txtCourseName;
+    public TextField txtStudentEmail;
+    public TextField txtCourseFee;
+    public Button btnSave;
+    public ComboBox cmbCourse;
+    public ComboBox cmbStudent;
+    public RadioButton rBtnCash;
+    public ToggleGroup paymentType;
 
-    public void btnOpenStudentFormOnAction(ActionEvent actionEvent) throws IOException {
-        setUi("StudentForm");
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("DashboardForm");
+    }
+
+    public void registerNowOnAction(ActionEvent actionEvent) {
+
     }
 
     public void setUi(String location) throws IOException {
@@ -21,17 +34,5 @@ public class DashboardFormController {
         stage.setScene((new Scene(FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml")))));
         stage.setTitle(location);
         stage.centerOnScreen();
-    }
-
-    public void btnOpenCourseFormOnAction(ActionEvent actionEvent) throws IOException {
-        setUi("CourseForm");
-    }
-
-    public void btnOpenRegistrationFormOnAction(ActionEvent actionEvent) throws IOException {
-        setUi("RegistrationForm");
-    }
-
-    public void btnOpenAllRegistrationFormOnAction(ActionEvent actionEvent) {
-
     }
 }
