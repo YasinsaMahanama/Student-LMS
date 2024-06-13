0550299f84fd721717b7a24ee4a434548c60497c
Yasinsa Mahanama <yasinsamahanama@gmail.com>
2024-06-14 00:01:48 +0530

    updated

---
+++ src/com/devstack/lms/controller/RegistrationDetailsFormController.java
@@ -0,0 +1,24 @@
+package com.devstack.lms.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class RegistrationDetailsFormController {
+
+    public AnchorPane context;
+    public TextField txtStudentName;
+    public TextField txtAddress;
+    public TextField txtAge;
+    public TextField txtEmail;
+    public TextField txtCourseName;
+    public TextField txtFee;
+    public TextField txtDate;
+    public TextField txtPaymentType;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void printOnAction(ActionEvent actionEvent) {
+    }
+}
